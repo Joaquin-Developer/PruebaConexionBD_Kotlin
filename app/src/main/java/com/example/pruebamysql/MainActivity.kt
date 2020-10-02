a61c574379de7b8c9fa56c300c62faa3f545670e
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-10-02 20:00:36 -0300

    agrego referencias a xml y clase ControladorBD

--- app/src/main/java/com/example/pruebamysql/MainActivity.kt
+++ app/src/main/java/com/example/pruebamysql/MainActivity.kt
@@ -1,11 +1,20 @@
-initpackage com.example.pruebamysql
+package com.example.pruebamysql
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import kotlinx.android.synthetic.main.activity_main.*   // se importa los elementos de xml
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* evento Click del Boton btnConexion */
+        btnConexion.setOnClickListener() {
+
+        }
+
+
     }
 }
