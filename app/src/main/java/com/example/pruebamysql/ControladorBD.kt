487736c36ca0aa76ae19f2cf6df7ef32f8ccc12f
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-10-02 20:38:49 -0300

    realizo la base de la conexionBD y muestro resultado de query en Toast

--- app/src/main/java/com/example/pruebamysql/ControladorBD.kt
+++ app/src/main/java/com/example/pruebamysql/ControladorBD.kt
@@ -1,8 +1,35 @@
 package com.example.pruebamysql
 
-class ControladorBD {
+import java.sql.Connection
+import java.sql.DriverManager;
 
-    
+class ControladorBD {
+    var conexion: Connection? = null
+    val driver = "com.mysql.jdbc.Driver"
+    val url = "jdbc:mysql://localhost:3306/coronavirusUY"
+    val user = "root"
+    val password = ""
+
+    fun getConnection(): Connection? {
+        Class.forName(driver)
+        conexion = DriverManager.getConnection(url, user, password)
+        return conexion
+    }
+
+    @Throws(Exception::class)
+    fun obtenerUnDato(): List<String> {
+        var lista = mutableListOf<String>()
+        val con = getConnection()
+        val st = con!!.createStatement()
+        val result = st.executeQuery("select nombre from usuarios")
+        while (result.next()) {
+            lista.add(result.getString("nombre"))
+        }
+        con.close()
+        return lista
+    }
+
+
 
 
 }
--- app/src/main/java/com/example/pruebamysql/MainActivity.kt
+++ app/src/main/java/com/example/pruebamysql/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.pruebamysql
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*   // se importa los elementos de xml
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +12,12 @@ class MainActivity : AppCompatActivity() {
 
         /* evento Click del Boton btnConexion */
         btnConexion.setOnClickListener() {
+            val conexion = ControladorBD()
+            var lista: List<String> = conexion.obtenerUnDato()
+
+            for (elem in lista) {
+                Toast.makeText(this, elem.toString(), Toast.LENGTH_SHORT).show()
+            }
 
         }
 
